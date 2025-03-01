35dd7582ac57223c50404a5a6ab122bb69af62cc
bytedance <yuanfeng634@gmail.com>
2025-03-01 21:41:40 +0800

    Add nvim
    Change-Id: Ie2f306da208bf2c8d24a14a9bc70f6d0268b5ec6

--- home/nvim.nix
+++ home/nvim.nix
@@ -10,7 +10,7 @@
   xdg.configFile."nvim" = {
     source = builtins.fetchGit {
       url = "https://github.com/dyike/nvimrc.git";
-      ref = "main";
+    #   ref = "master";
     };
     recursive = true;
   };
