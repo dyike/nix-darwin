25fafe4968aad30e2f6b9ce65701688880563231
ityike <ityike@mac-mini.localdomain>
2025-10-07 23:16:01 +0800

    fix flake nix

--- modules/nix-core.nix
+++ modules/nix-core.nix
@@ -4,14 +4,13 @@
   nix.settings = {
     # enable flakes globally
     experimental-features = ["nix-command" "flakes"];
-
+    ssl-cert-file = "/etc/ssl/cert.pem";
     # substituers that will be considered before the official ones(https://cache.nixos.org)
     substituters = [
-      "https://mirror.sjtu.edu.cn/nix-channels/store"
-      "https://nix-community.cachix.org"
+      "https://cache.nixos.org"
     ];
     trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
     builders-use-substitutes = true;
   };
